9ccbad32d5b14e9672dc154074aae04e944071ec
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-05-10 14:52:06 -0400

    Refactor into import mei

--- mei2volpiano.py
+++ mei2volpiano.py
@@ -1,14 +1,14 @@
 # Skeleton Process
-# 1. Get the MEI file into the script 
-# 2. Parse until the `body` section is reached 
+# 1. Get the MEI file into the script
+# 2. Parse until the `body` section is reached
 # 3. Parse each `sylabble` tag and put the words and neuemes in pairs (dict)
 # 4. Match the words and the neumes at the end of the body loop
 # 5. Convert output dict into string and export
 # Process is one pass with O(x) for x = length of lines in body. Roughly
 import sys
 
+
 class MEItoVolpiano:
-    
     def import_mei(mei_file):
 
         bodyFlag = bodyEndFlag = False
@@ -19,11 +19,7 @@ class MEItoVolpiano:
                 bodyEndFlag = True
             if bodyFlag and not bodyEndFlag:
                 print(line.strip())
-       
-     
 
-
-    
     def create_volpaino(parsed_mei):
         pass
 
@@ -34,8 +30,7 @@ class MEItoVolpiano:
 def main():
     f = open(sys.argv[1], "r")
     MEItoVolpiano.import_mei(f)
-    
-    
+
 
 if __name__ == "__main__":
     main()
