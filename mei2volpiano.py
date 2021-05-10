4772d88ed932ef3b183916c0201d21a480363920
raviraina <ravi.raina@brentwood.ca>
2021-05-10 13:53:54 -0400

    added boilerplate

--- mei2volpiano.py
+++ mei2volpiano.py
@@ -5,3 +5,25 @@
 # 4. Match the words and the neumes at the end of the body loop
 # 5. Convert output dict into string and export
 # Process is one pass with O(x) for x = length of lines in body. Roughly
+
+class MEItoVolpiano:
+    
+    def import_mei(self, mei_file):
+        print(mei_file)
+
+    def parse_mei(self, mei_body):
+        pass
+    
+    def create_volpaino(self, parsed_mei):
+        pass
+
+    def export_volpiano(self, volpiano_file):
+        pass
+
+
+def main():
+    a = MEItoVolpiano()
+    a.import_mei(2)
+
+if __name__ == "__main__":
+    main()
