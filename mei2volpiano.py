0f4bb2b8e6b8a6d827bb4b2ffa0bc5259bad30ca
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-05-11 00:58:37 -0400

    Moved to parser. Not working yet.

--- mei2volpiano.py
+++ mei2volpiano.py
@@ -7,57 +7,25 @@
 # Process is one pass with O(x) for x = length of lines in body. Roughly
 import sys
 import argparse
+import xml.etree.ElementTree as ET
 
 
 class MEItoVolpiano:
-    def import_mei(mei_file):
-
-        # The two flags find the start and end of the body tag, printing lines between them.
-        mei_line_array = []
-        bodyFlag = bodyEndFlag = False
-        for line in mei_file:
-            if "<body" in line:
-                bodyFlag = True
-            if "</body" in line:
-                bodyEndFlag = True
-            if bodyFlag and not bodyEndFlag:
-                # print(line.strip())
-                mei_line_array.append(line.strip())
-
-        return mei_line_array
-
     def find_clef(parsed_mei):
 
-        # Find the clef shape and line location from body
-        clef = []
-        for line in parsed_mei:
-            if "staffDef" in line: # This assumes exact information location 
-                curr = line.split() # TODO Secure this for manual locations 
-                clef.append(curr[-1])
-                clef.append(curr[-2])
-                break
+        root = tree.getroot()
+        for body in root.findall("{http://www.music-encoding.org/ns/mei}body"):
+        staff = body.find("{http://www.music-encoding.org/ns/mei}staffDef")
+        print(staff.text)
 
-        for char in clef[0]:
-            if char >= "A" and char <= "Z":
-                clef[0] = char
-        for char in clef[1]:
-            if char.isdigit():
-                clef[1] = int(char)
-        return clef
-
-    def export_volpiano(volpiano_file):
-        pass
 
 
 def main():
-    f = open(sys.argv[1], "r")
-    clean_mei = MEItoVolpiano.import_mei(f)
+    tree = ET.parse(sys.argv[1])
 
-    # for line in clean_mei:
-    #    print(line)
+    clef = find_clef(tree)
 
-    clef = MEItoVolpiano.find_clef(clean_mei)
-    print(clef)
+    
 
 
 if __name__ == "__main__":
