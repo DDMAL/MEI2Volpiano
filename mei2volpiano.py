22d6342b56b82ae13cc5db7c28d7bb4b52da49ad
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-05-11 11:03:01 -0400

    Removed clef

--- mei2volpiano.py
+++ mei2volpiano.py
@@ -6,7 +6,6 @@
 # 5. Convert output dict into string and export
 # Process is one pass with O(x) for x = length of lines in body. Roughly
 
-import re
 import argparse
 import xml.etree.ElementTree as ET
 
@@ -23,25 +22,7 @@ class MEItoVolpiano:
 
     def find_clef(parsed_mei):
 
-        # Find the clef shape and line location from body
-        clef = []
-        for line in parsed_mei:
-            if "staffDef" in line:  # This assumes exact information location
-
-                # playing around with some regex
-                # can be used for other lines as well
-                mei_tag_attrs = dict(re.findall(r"(\w*)=(\".*?\"|\S*)", line))
-                clef.append(mei_tag_attrs["shape"])
-                clef.append(mei_tag_attrs["line"])
-                break
-
-        for char in clef[0]:
-            if "A" <= char <= "Z":
-                clef[0] = char
-        for char in clef[1]:
-            if char.isdigit():
-                clef[1] = int(char)
-        return clef
+        pass
 
     def export_volpiano(volpiano_file):
         pass
