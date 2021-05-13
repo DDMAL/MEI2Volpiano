9f61c093ad8a8cc0cb09cfbf104243089430d250
raviraina <ravi.raina@brentwood.ca>
2021-05-13 11:21:57 -0400

    revamped argparse

--- driver.py
+++ driver.py
@@ -8,38 +8,68 @@ from timeit import default_timer as timer
 def main():
     start = timer()
     parser = argparse.ArgumentParser()
+    option = parser.add_mutually_exclusive_group(required=True)
 
     # check the validity of file(s) being passed into program
-    def check_file_validity(fname):
+    def check_file_validity(fname, valid_ext):
         ext = os.path.splitext(fname)[1][1:]
-        if ext != "mei":
+        if ext != valid_ext:
             parser.error('Must be a valid mei file with an ".mei" extension')
         return fname
 
-    parser.add_argument(
-        "mei_files",
-        type=lambda fname: check_file_validity(fname),
+    option.add_argument(
+        "-mei",
+        type=lambda fname: check_file_validity(fname, "mei"),
         nargs="+",
         help="An MEI encoded music file",
     )
 
-    parser.add_argument(
-        "--e", type=str, nargs="?", help="A text file to hold output volpiano strings"
+    option.add_argument(
+        "-txt",
+        nargs="?",
+        type=lambda fname: check_file_validity(fname, "txt"),
+        help="A text file with each MEI file/path to be converted per line",
     )
+
+    parser.add_argument(
+        "-export",
+        action="store_true",
+        help="flag output converted volpiano to a .txt file (name corresponding with input)",
+    )
+
     args = vars(parser.parse_args())  # stores each positional input in dict
 
     lib = mei2volpiano.MEItoVolpiano()
-    ind = 1
-    for mei_file in args["mei_files"]:
-        with open(mei_file, "r") as f:
-            print("\n" + f"The corresponding Volpiano string for {mei_file} is:")
-            final_string = lib.convert_mei_volpiano(f)
-            print("\n" + final_string + "\n")
-        if args["e"] is not None:
-            with open(f'{ind}_{args["e"]}', "a") as out:
-                out.write(mei_file + "\n")
-                out.write(final_string + "\n")
-        ind += 1
+    vol_strings = []
+    f_names = []
+
+    if args["mei"] and args["txt"]:
+        parser.error('Cannot use both "-mei" and "-txt" simultaneously')
+
+    if args["txt"] is not None:
+        txt_file = open(args["txt"])
+        for mei_file in txt_file:
+            f_names.append(mei_file.strip())
+            vol_strings.append(lib.convert_mei_volpiano(mei_file.strip()))
+
+    if args["mei"] is not None:
+        for mei_file in args["mei"]:
+            with open(mei_file, "r") as f:
+                f_names.append(mei_file)
+                vol_strings.append(lib.convert_mei_volpiano(f))
+
+    name_vol_pairs = list(zip(f_names, vol_strings))
+
+    if args["export"]:
+        for pair in name_vol_pairs:
+            basename = os.path.basename(pair[0])
+            out_name = os.path.splitext(basename)[0]
+            with open(f"{out_name}.txt", "a") as out:
+                out.write(out_name + "\n")
+                out.write(pair[1])
+
+    for pair in name_vol_pairs:
+        print( f"\nThe corresponding Volpiano string for {pair[0]} is: \n{pair[1]}\n")
 
     # testing time
     elapsed_time = timer() - start
