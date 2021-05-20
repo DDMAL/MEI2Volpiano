f6281519a13d87de13780b47f8a4ad0a8fcbd598
raviraina <ravi.raina@brentwood.ca>
2021-05-20 10:19:43 -0400

    default val causes positional errors, removed for current version

--- mei2volpiano/driver.py
+++ mei2volpiano/driver.py
@@ -44,8 +44,8 @@ def main():
 
     parser.add_argument(
         "t",
-        default="mei",
-        const="mei",
+        # default="mei",
+        # const="mei",
         nargs="?",
         choices=["txt", "mei"],
         help="Choice indicating whether the inputs will be mei or txt files",
